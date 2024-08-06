d5998509cff73bb5753f21c4bb5633d0873eee2d
DASARI YOGESWAR REDDY <116977172+yogeswarreddy14@users.noreply.github.com>
2024-08-06 12:06:26 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("Enter first element : ");
+	scanf("%d",&a);
+	printf("Enter the second number : ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("The addition of two numbers =%d ",c);
+	return 0;
+
+	
+}
