17f7b0a1deddd1007861a3e32d9e6f7687f93a8a
DASARI YOGESWAR REDDY <116977172+yogeswarreddy14@users.noreply.github.com>
2024-08-19 09:42:53 +0530

    Add files via upload

---
+++ christmas cc.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+	int x;
+	scanf("%d",&x);
+	    if(x==25){
+	        printf("christmas");
+	    }
+	    else{
+	        printf("ordinary");
+	    }
+	
+	return 0;
+
+}
+
+
