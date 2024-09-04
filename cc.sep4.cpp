35152c2ffbcf207553108edf0a239b74316b6e94
DASARI YOGESWAR REDDY <116977172+yogeswarreddy14@users.noreply.github.com>
2024-09-04 21:38:50 +0530

    Add files via upload

---
+++ cc.sep4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int x;
+	scanf("%d",&x);
+	if(x>=1&&x<=8){
+	    printf("YES\n");
+	}
+	else{
+	    printf("NO\n");
+	}
+	return 0;
+
+}
+
+x
