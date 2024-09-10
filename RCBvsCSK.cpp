e3f31491e6e8010cc129522999fdacf35f377218
DASARI YOGESWAR REDDY <116977172+yogeswarreddy14@users.noreply.github.com>
2024-09-10 22:44:41 +0530

    Add files via upload

---
+++ RCBvsCSK.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int x,y;
+	scanf("%d %d",&x,&y);
+	if(x>y&&x>=18){
+	    printf("RCB\n");
+	}
+	    
+	    else{
+	        printf("CSK\n");
+	        }
+
+	
+	return 0;
+
+}
+
+
