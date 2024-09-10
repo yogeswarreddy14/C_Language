0d579f2b087ed6808fccb7d2181d0e4cb12894f0
DASARI YOGESWAR REDDY <116977172+yogeswarreddy14@users.noreply.github.com>
2024-09-10 22:45:05 +0530

    Add files via upload

---
+++ tour of king.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n,m,s;
+	    scanf("%d %d",&n,&m);
+	    s=n*5+m*7;
+	    printf("%d\n",&s);
+	}
+	return 0;
+
+}
+
+
